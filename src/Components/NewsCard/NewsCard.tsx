1b1df94b5a0cbecd16bd950905823de0d033e4be
sscodes <sanketcrj@gmail.com>
2024-05-20 03:16:16 +0530

    news-card UI fix

--- src/Components/NewsCard/NewsCard.tsx
+++ src/Components/NewsCard/NewsCard.tsx
@@ -5,7 +5,8 @@ const NewsCard = ({ news = { text: '', author: '' } }) => {
     <div className='news-card'>
       <div className='news-card__title'>
         <a href={news?.url} target='_blank' className='news-card__new-title'>
-          {news?.title}
+          {news?.title.slice(0, 74)}
+          {news?.title.length >= 74 && '...'}
         </a>
       </div>
       <div className='news-card__author'>
