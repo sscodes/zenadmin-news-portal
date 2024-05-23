6c45744c4f4610b40cb650f2699030307b6c4b5e
sscodes <sanketcrj@gmail.com>
2024-05-23 13:12:21 +0530

    updated hosted api

--- client/src/API/API.ts
+++ client/src/API/API.ts
@@ -2,17 +2,17 @@ import { NewsListsType, NewsDetails } from '../Types/Type';
 
 export const fetchSearchResults = async (key: string, page: number) => {
   const res = await fetch(
-    `http://localhost:4000/api/search?query=${key}&page=${page}`
+    `https://zenadmin-news-portal-server.onrender.com/api/search?query=${key}&page=${page}`
   );
   return (await res.json()) as NewsListsType;
 };
 
 export const fetchLatestNews = async () => {
-  const res = await fetch('http://localhost:4000/api/latest-news');
+  const res = await fetch('https://zenadmin-news-portal-server.onrender.com/api/latest-news');
   return (await res.json()) as NewsListsType;
 };
 
 export const fetchNewsDetail = async (id: number) => {
-  const res = await fetch(`http://localhost:4000/api/news-details/${id}`);
+  const res = await fetch(`https://zenadmin-news-portal-server.onrender.com/api/news-details/${id}`);
   return (await res.json()) as NewsDetails;
 };
